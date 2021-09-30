0e2a0f31f5cca90e131380d1b267f22cd67097f6
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2021-09-30 11:50:25 +0100

    ACU-660: Add linters and github actions

--- CRM/Custommosaico/Page/EditorIframe.php
+++ CRM/Custommosaico/Page/EditorIframe.php
@@ -6,7 +6,7 @@ class CRM_Custommosaico_Page_EditorIframe extends CRM_Mosaico_Page_Editor {
     $smarty = CRM_Core_Smarty::singleton();
     $plugins = [];
 
-    // Allow plugins to be added by a hook
+    // Allow plugins to be added by a hook.
     if (class_exists('\Civi\Core\Event\GenericHookEvent')) {
       \Civi::dispatcher()->dispatch('hook_civicrm_mosaicoPlugin',
         \Civi\Core\Event\GenericHookEvent::create([
@@ -15,7 +15,7 @@ class CRM_Custommosaico_Page_EditorIframe extends CRM_Mosaico_Page_Editor {
       );
     }
 
-    $smarty->assign('mosaicoPlugins', '[ ' . implode( ',', $plugins ) . ' ]');
+    $smarty->assign('mosaicoPlugins', '[ ' . implode(',', $plugins) . ' ]');
     parent::run();
   }
 
