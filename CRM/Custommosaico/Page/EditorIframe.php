762f0d9582e7babeafc4383126279a846c9daffc
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2022-01-19 13:47:34 +0100

    ACU-659: Add hooks to add css and js to Mosaico

--- CRM/Custommosaico/Page/EditorIframe.php
+++ CRM/Custommosaico/Page/EditorIframe.php
@@ -19,4 +19,30 @@ class CRM_Custommosaico_Page_EditorIframe extends CRM_Mosaico_Page_Editor {
     parent::run();
   }
 
+  /**
+   * Modify return value of parent:: method.
+   */
+  protected function getScriptUrls() {
+    $scriptUrls = parent::getScriptUrls();
+
+    CRM_Utils_Hook::singleton()->invoke(['scriptUrls'], $scriptUrls, $null, $null,
+      $null, $null, $null,
+      'civicrm_mosaicoScriptUrlsAlter'
+    );
+    return $scriptUrls;
+  }
+
+  /**
+   * Modify return value of parent:: method.
+   */
+  protected function getStyleUrls() {
+    $styleUrls = parent::getStyleUrls();
+
+    CRM_Utils_Hook::singleton()->invoke(['styleUrls'], $styleUrls, $null, $null,
+      $null, $null, $null,
+      'civicrm_mosaicoStyleUrlsAlter'
+    );
+    return $styleUrls;
+  }
+
 }
